ef409bcdddeb82aa9c7a083099b356190312a4f1
ExTeeP <exteep@yandex.ru>
2020-06-27 14:20:47 +0700

    Валидация хештегов

--- js/main.js
+++ js/main.js
@@ -11,6 +11,11 @@ var SCALE_MAX = 100;
 // Максимальное колличество символов в описании к фото
 var MAX_LENGTH_DESC = 140;
 
+// Валидация хештегов
+var HASHTAG_REGEXP = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+var MAX_HASHTAGS = 5;
+var MAX_HASHTAG_CHARACTERS = 20;
+
 // Массив имен
 var USERS = [
   'Александр',
@@ -110,6 +115,9 @@ var saturationPin = pictureEditModal.querySelector('.effect-level__pin');
 var saturationValue = pictureEditModal.querySelector('.effect-level__value');
 var saturationLine = pictureEditModal.querySelector('.effect-level__line');
 var saturationLineDepth = pictureEditModal.querySelector('.effect-level__depth');
+
+// Инпут хештегов
+var pictureHashtag = pictureEditModal.querySelector('.text__hashtags');
 
 // Поле описания фотографии
 var pictureDescription = pictureEditModal.querySelector('.text__description');
@@ -328,8 +336,12 @@ pictureUploadInput.addEventListener('change', function () {
   pictureEffectList.addEventListener('click', onEffectPreviewClick);
 
   pictureDescription.addEventListener('change', onPicteruDescriptionChange);
-  pictureDescription.addEventListener('focus', onPicteruDescriptionFocus);
-  pictureDescription.addEventListener('blur', onPictureDescriptionBlur);
+  pictureDescription.addEventListener('focus', onElementFocus);
+  pictureDescription.addEventListener('blur', onElementBlur);
+
+  pictureHashtag.addEventListener('change', onPictureHashtragChange);
+  pictureHashtag.addEventListener('focus', onElementFocus);
+  pictureHashtag.addEventListener('blur', onElementBlur);
 });
 
 // Обработчик закрытия модального окна редактирования фотографии
@@ -344,8 +356,12 @@ pictureEditClose.addEventListener('click', function (evt) {
   pictureEffectList.removeEventListener('click', onEffectPreviewClick);
 
   pictureDescription.removeEventListener('change', onPicteruDescriptionChange);
-  pictureDescription.removeEventListener('focus', onPicteruDescriptionFocus);
-  pictureDescription.removeEventListener('blur', onPictureDescriptionBlur);
+  pictureDescription.removeEventListener('focus', onElementFocus);
+  pictureDescription.removeEventListener('blur', onElementBlur);
+
+  pictureHashtag.removeEventListener('change', onPictureHashtragChange);
+  pictureHashtag.removeEventListener('focus', onElementFocus);
+  pictureHashtag.removeEventListener('blur', onElementBlur);
 });
 
 /* ================================================================================= */
@@ -474,13 +490,80 @@ function onPicteruDescriptionChange(evt) {
 }
 
 // Когда поле в фокусе закрыть попап нельзя
-function onPicteruDescriptionFocus() {
+function onElementFocus() {
   document.removeEventListener('keydown', onModalEscPress);
 }
 
 // Когда поле теряет фокус, его можно закрыть
-function onPictureDescriptionBlur() {
+function onElementBlur() {
   document.addEventListener('keydown', onModalEscPress);
 }
 
 /* ================================================================================= */
+
+// Возвращает массив введенных хештегов
+function createHashtags(input) {
+  return input.value.toLowerCase().split(' ');
+}
+
+// Избавляет массив хештегов от лишних пробелов
+function removeEmptyHashtag(hashtagsArr) {
+  var notEmptyHashtags = [];
+
+  hashtagsArr.forEach(function (hastag) {
+    if (hastag !== '') {
+      notEmptyHashtags.push(hastag);
+    }
+  });
+
+  return notEmptyHashtags;
+}
+
+// Добавляет сообщение об ошибке в массив с ошибками
+var pushErrorMessage = function (errorMessage, errorMessagesArr) {
+  if (errorMessagesArr.indexOf(errorMessage) === -1) {
+    errorMessagesArr.push(errorMessage);
+  }
+};
+
+// Проверяет хештеги на наличие ошибок
+var createValidityMessages = function (notEmptyHashtags) {
+  var errorMessages = [];
+
+  if (notEmptyHashtags.length > MAX_HASHTAGS) {
+    pushErrorMessage('Хеш-тегов не должно быть больше ' + MAX_HASHTAGS + ' .', errorMessages);
+  }
+
+  notEmptyHashtags.forEach(function (hashtag) {
+    if (!hashtag.startsWith('#')) {
+      pushErrorMessage('Хеш-тег должен начинаться с символа решетки (#).', errorMessages);
+    } else if (hashtag.length === 1) {
+      pushErrorMessage('Хеш-тег не может состоять из одного символа.', errorMessages);
+    } else if (hashtag.length > MAX_HASHTAG_CHARACTERS) {
+      pushErrorMessage('Хеш-тег не может состоять из более чем ' + MAX_HASHTAG_CHARACTERS + ' символов.', errorMessages);
+    } else if (!hashtag.match(HASHTAG_REGEXP)) {
+      pushErrorMessage('Хеш-тег должен состоять только из букв и цифр.', errorMessages);
+    } else if (notEmptyHashtags.indexOf(hashtag) !== notEmptyHashtags.lastIndexOf(hashtag)) {
+      pushErrorMessage('Хеш-теги не должны повторяться.', errorMessages);
+    }
+  });
+
+  return errorMessages;
+};
+
+// Показывает сообщения об ошибках
+function showValidityErrors(evt, hashtagsArr) {
+  var errors = createValidityMessages(hashtagsArr);
+  if (errors.length !== 0) {
+    evt.target.setCustomValidity(errors.join('\n'));
+  } else {
+    evt.target.setCustomValidity('');
+  }
+}
+
+// Валидирует хештеги
+function onPictureHashtragChange(evt) {
+  var allHashtags = createHashtags(evt.target);
+  var correctHashtags = removeEmptyHashtag(allHashtags);
+  showValidityErrors(evt, correctHashtags);
+}
